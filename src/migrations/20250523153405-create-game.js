9f84017865a300fbf7c2cedc9cc098cabe2b1c8d
jafar-sweity <jata1518@gmail.com>
2025-05-23 18:50:39 +0300

    feat: add Game model migration

---
+++ src/migrations/20250523153405-create-game.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Games", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      gradeId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: "Grades",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Games");
+  },
+};
